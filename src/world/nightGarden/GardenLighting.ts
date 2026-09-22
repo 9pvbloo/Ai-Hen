aa60721c81b4ae5860f2db8ef618d77817ee7146
9pvbloo <i2413131@continental.edu.pe>
2026-09-22 00:09:55 -0500

    feat: refine moonlit garden light hierarchy

--- src/world/nightGarden/GardenLighting.ts
+++ src/world/nightGarden/GardenLighting.ts
@@ -1,36 +1,26 @@
-import { DirectionalLight, HemisphereLight, Object3D, PointLight } from 'three'
+import { DirectionalLight, HemisphereLight, Object3D } from 'three'
 import type { Group } from 'three'
 
 export class GardenLighting {
-  private readonly sky = new HemisphereLight('#6f929b', '#07100f', 0.23)
-  private readonly moon = new DirectionalLight('#dbecee', 3.05)
-  private readonly rockRim = new DirectionalLight('#9bbbc2', 0.34)
-  private readonly pathLanterns = [
-    new PointLight('#e2a24a', 0.72, 5.8, 2), new PointLight('#e2a24a', 0.56, 4.8, 2),
-    new PointLight('#d98d34', 0.42, 4.4, 2),
-  ]
+  private readonly sky = new HemisphereLight('#6f929b', '#07100f', 0.18)
+  private readonly moon = new DirectionalLight('#dbecee', 3.35)
+  private readonly rockRim = new DirectionalLight('#9bbbc2', 0.18)
   private readonly target = new Object3D()
   private readonly rimTarget = new Object3D()
 
   constructor(parent: Group) {
-    this.moon.position.set(-12, 15, 4)
-    this.target.position.set(-0.8, -4.15, -24)
+    this.moon.position.set(-14, 16, 3)
+    this.target.position.set(0.8, -3.5, -32)
     this.moon.target = this.target
-    this.rockRim.position.set(8, 5, -4)
-    this.rimTarget.position.set(-5.1, -3.2, -21.4)
+    this.rockRim.position.set(7, 5, -5)
+    this.rimTarget.position.set(-3.4, -3.1, -24)
     this.rockRim.target = this.rimTarget
-    this.pathLanterns[0].position.set(-3.05, -3.65, -14.95)
-    this.pathLanterns[1].position.set(-1.25, -3.72, -27.95)
-    this.pathLanterns[2].position.set(0.75, -3.65, -39.15)
-    parent.add(this.sky, this.moon, this.rockRim, this.target, this.rimTarget, ...this.pathLanterns)
+    parent.add(this.sky, this.moon, this.rockRim, this.target, this.rimTarget)
   }
 
   setIntensity(visibility: number): void {
-    this.sky.intensity = 0.23 * visibility
-    this.moon.intensity = 3.05 * visibility
-    this.rockRim.intensity = 0.34 * visibility
-    this.pathLanterns[0].intensity = 0.72 * visibility
-    this.pathLanterns[1].intensity = 0.56 * visibility
-    this.pathLanterns[2].intensity = 0.42 * visibility
+    this.sky.intensity = 0.18 * visibility
+    this.moon.intensity = 3.35 * visibility
+    this.rockRim.intensity = 0.18 * visibility
   }
 }
